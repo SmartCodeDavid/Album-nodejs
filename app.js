b72625addbc038548e2ca2020017c297f2157e71
david <tlan8029@uni.usyd.edu.au>
2018-05-04 04:17:36 +1000

    setting up basic function and nodejs can get folders and display on the client side.

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+//set basedir
+app.locals.basedir = __dirname;
+console.log(app.locals.basedir);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,19 +25,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
+  // res.render('page404');
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('page404');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   ////get dirctories
-  
-  res.render('index');
+  //var folderName = req.params.folderName;
+  var folders = [];
+  var callback = function (err, files) {
+    if (err)
+      throw err;
+    res.render('index', { folders: files });
+  }
+  console.log(process.cwd());
+  fs.readdir(process.cwd() + '/public/folders/', function (err, files) {
+    if (err)
+      throw err;
+    files.forEach(function (file, index) {
+      fs.stat(process.cwd() + '/public/folders/' + file, function (err, stats) {
+        if (err)
+          throw err;
+        if (stats.isDirectory) {
+          folders.push(file);
+          console.log(folders);
+        }
+        if (files.length == index + 1) {
+          callback(null, folders);
+        }
+      });
+    })
+  });
 });
 
-router.get('/upload', function(req, res){
+router.get('/upload', function (req, res) {
   res.render('upload');
 });
 
