2b2de5aa0cead05980538cb9ef1bb958f24bde5d
david <tlan8029@uni.usyd.edu.au>
2018-05-06 02:21:47 +1000

    upload improve

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,74 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var fileController = require('../controllers/FileController');
+var formidable = require('formidable'),
+  http = require('http'),
+  util = require('util');
+
+router.post('/upload', function(req, res){
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+
+    //Sets the directory for placing file uploads in  You can move them later on using fs.rename(). The default is os.tmpdir().
+    form.uploadDir = __dirname + "/../tmpUpload/";
+    console.log(form.uploadDir);
+    
+    //include the extensions of the original file(s)
+    form.type = true;
+
+    //2MB size maximun of uploaded file
+    form.maxFileSize = 2 * 1024 * 1024;
+    
+    form.multiples = true;
+
+    form.parse(req, function (err, fields, files) {
+      if(err) {
+        console.log(err);
+        res.writeHead(500, err);
+        res.write(err.message);
+        res.end();
+        return ;
+      }
+
+      var callback = function(err){
+        if(err) {
+          res.writeHead(500, err);
+          res.end(err);
+        }
+        //success -- upload
+        res.json({'result':'success'});
+      }
+      
+      //Store files to specific location indicated by users
+      var pathStoredFolder = process.cwd() + '/public/folders/' + fields.selectedFolder + '/';
+      if (files.upload instanceof (Array) ) {
+        files.upload.forEach(function(element, index) {
+          fs.rename(element.path, pathStoredFolder + (parseInt(Math.random()*1000)) + element.name, function(err){
+            if(err) {
+              console.log(err);
+              callback(err);
+              return ;
+            }
+            if(index == files.upload.length - 1) {
+              callback(null);
+            }
+          });
+        });
+      } else {
+        //files.upload
+        fs.rename(files.upload.path, pathStoredFolder + (parseInt(Math.random() * 1000)) + files.upload.name, function (err) {
+          if (err) {
+            console.log(err);
+            callback(err);
+            return;
+          }
+          callback(null);
+        });
+      }
+    });
+  }
+});
 
 router.get('/upload', function(req, res) {
   //get options
@@ -10,7 +78,6 @@ router.get('/upload', function(req, res) {
       throw err;
       return;
     }
-
     res.render('upload', {'selections' : files});
   });
 });
